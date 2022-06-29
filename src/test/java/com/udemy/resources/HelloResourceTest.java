654d35a56ba35660a6fd630d6437388b8d9e0a24
Jagadeesh Dachepalli <jagadeesh.dachepalli@oracle.com>
2022-06-29 22:40:18 +0530

    add authentication to unittest: not working, but we have added HTTPAuthFeature and BasicCredentials to generate auth and use setUpClass to register the auth before calling our required test method

--- src/test/java/com/udemy/resources/HelloResourceTest.java
+++ src/test/java/com/udemy/resources/HelloResourceTest.java
@@ -1,8 +1,17 @@
 package com.udemy.resources;
 
+import com.google.common.base.Optional;
+import com.udemy.core.User;
+import io.dropwizard.auth.AuthFactory;
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.basic.BasicAuthFactory;
+import io.dropwizard.auth.basic.BasicCredentials;
 import io.dropwizard.testing.junit.ResourceTestRule;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.glassfish.jersey.test.JerseyTest;
 //import org.junit.jupiter.api.*;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 
@@ -11,9 +20,24 @@ import javax.ws.rs.core.MediaType;
 //import static org.junit.jupiter.api.Assertions.*;
 
 public class HelloResourceTest {
+    private static final Authenticator<BasicCredentials, User> AUTHENTICATOR = new Authenticator<BasicCredentials, User>() {
+        @Override
+        public Optional<User> authenticate(BasicCredentials basicCredentials) throws AuthenticationException {
+            return Optional.of(new User());
+        }
+    };
 
+    private static final HttpAuthenticationFeature FEATURE = HttpAuthenticationFeature.basic("user", "password");
     @ClassRule
-    public static final ResourceTestRule RULE = ResourceTestRule.builder().addResource(new HelloResource()).build();
+    public static final ResourceTestRule RULE = ResourceTestRule.builder().addProvider(
+            AuthFactory.binder(
+                    new BasicAuthFactory<>(
+                            AUTHENTICATOR,
+                            "realm",
+                            User.class
+                    )
+            )
+    ).addResource(new HelloResource()).build();
 
     @Test
     public void getGreeting() {
@@ -22,6 +46,10 @@ public class HelloResourceTest {
         assert expected.equals(actual);
     }
 
+    @Before
+    public void setUpClass(){
+        RULE.getJerseyTest().client().register(FEATURE);
+    }
     @Test
     public void getSecuredGreeting() {
         String expected = "HelloSecuredWorld!!";
