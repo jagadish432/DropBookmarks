a3b0bd64a48df0f22eeeb49bde8686a89874818f
Jagadeesh Dachepalli <jagadeesh.dachepalli@oracle.com>
2022-07-06 23:12:28 +0530

    configure database connection, add dropwizard-db package, add database related config in config.yml, update README.md file

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ keytool -genkeypair -keyalg RSA -dname "CN=localhost" -keystore dropbookmarks.ke
 
 * we need to provide/set the key password and store password.
 
+## For adding new dependencies
+
+. Add required dependencies in the pom.xml file
+
+. Then, run mvn clean install
+
+. Once the build is successful from the above command, then try to access the library/package specific function,variable,class, etc.. if IDE shows it then good , 
+otherwise place the cursor on it and do opt+enter to search in maven dependency and add it. Also, tru the "reload from disk and rebuild the project", sometimes it also works fine.
+
+
+
--- src/main/java/com/udemy/DropBookmarksConfiguration.java
+++ src/main/java/com/udemy/DropBookmarksConfiguration.java
@@ -2,12 +2,23 @@ package com.udemy;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 public class DropBookmarksConfiguration extends Configuration {
-    @NotEmpty
+    @org.hibernate.validator.constraints.NotEmpty
     private String password;
+
+    @NotNull
+    @Valid
+    private DataSourceFactory dataSourceFactory = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
+    }
 
     @JsonProperty
     public String getPassword() {
