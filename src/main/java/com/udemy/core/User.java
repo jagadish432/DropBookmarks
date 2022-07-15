3fb8c162754b7cc40532b907d0dc060bedb5e713
Jagadeesh Dachepalli <jagadeesh.dachepalli@oracle.com>
2022-07-15 22:48:42 +0530

    Map Java classes to database entities, and write queries to fetch from database

--- src/main/java/com/udemy/core/User.java
+++ src/main/java/com/udemy/core/User.java
@@ -1,5 +1,106 @@
 package com.udemy.core;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+@Entity
+@Table(name="users")
+@NamedQueries({
+        @NamedQuery(
+                name="com.udemy.core.User.findAll",
+                query = "select u from User u"
+        ),
+        @NamedQuery(
+                name="com.udemy.core.User.findByUsernamePassword",
+                query = "select u from User u" + " where u.username= :username" +" and u.password= :password"
+        )
+}
+)
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username") // not required here, but just to showcase we can use like this to map class attribute's to table's column names respectively
+    private String username;
+
+    private String password;
+
+    private List<Bookmark> bookmarks = new ArrayList<>();
+
+
+    public User() {
+    }
+
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Bookmark> getBookmarks() {
+        return bookmarks;
+    }
+
+    public void setBookmarks(List<Bookmark> bookmarks) {
+        this.bookmarks = bookmarks;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 59 * hash + Objects.hashCode(this.username);
+        hash = 59 * hash + Objects.hashCode(this.password);
+        return hash;
+    }
 }
