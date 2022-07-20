bf41f55c43ee944dd95d7edaca6efab6fc039b1a
Jagadeesh Dachepalli <jagadeesh.dachepalli@oracle.com>
2022-07-20 23:36:43 +0530

    add multiple APIs - create/edit/delete/get individual bookmarks, delete all bookmarks of the loggedin users'

--- src/main/java/com/udemy/resources/BookmarksResource.java
+++ src/main/java/com/udemy/resources/BookmarksResource.java
@@ -6,7 +6,9 @@ import com.udemy.core.User;
 import com.udemy.db.BookmarkDAO;
 import io.dropwizard.auth.Auth;
 import io.dropwizard.hibernate.UnitOfWork;
+import io.dropwizard.jersey.params.LongParam;
 
+import javax.validation.constraints.Null;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -37,4 +39,58 @@ public class BookmarksResource {
 
         return result;
     }
+
+    @GET
+    @UnitOfWork
+    @Path("/{id}")
+    // eg: https://localhost:8443/bookmark/1
+    public Optional<Bookmark> getBookmark(@PathParam("id")LongParam id
+            , @Auth User user) {
+        return findIfAuthorized(id.get(), user.getId());
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @UnitOfWork
+    public Optional<Bookmark> deleteBookmark(@PathParam("id")LongParam id,
+                                             @Auth User user) {
+        Optional<Bookmark> bookmark = findIfAuthorized(id.get(), user.getId());
+        if (bookmark.isPresent()) {
+            bookmarkDAO.delete(id.get());
+        }
+        return bookmark;
+    }
+
+    @POST
+    @UnitOfWork
+    public Bookmark saveBookmark(Bookmark bookmark, @Auth User user) {
+        bookmark.setUser(user);
+        return bookmarkDAO.save(bookmark);
+    }
+
+    @DELETE
+    @UnitOfWork
+    public List<Bookmark> deleteAllBookmarks(@Auth User user) {
+        List<Bookmark> bookmarks = bookmarkDAO.findForUser(user.getId());
+        if ( !bookmarks.isEmpty() ) {
+            bookmarkDAO.deleteBookmarksOfUser(user.getId());
+        }
+        return bookmarks;
+    }
+
+    @PUT
+    @UnitOfWork
+    @Path("/{id}")
+    public Optional<Bookmark> updateBookmark(@PathParam("id")LongParam id, Bookmark bookmark,
+                                             @Auth User user) {
+        Optional<Bookmark> bookmarkFromDB = findIfAuthorized(id.get(), user.getId());
+        if (bookmarkFromDB.isPresent()) {
+            bookmarkFromDB.get().setName(bookmark.getName());
+            bookmarkFromDB.get().setDescription(bookmark.getDescription());
+            bookmarkFromDB.get().setUrl(bookmark.getUrl());
+            return Optional.fromNullable(bookmarkDAO.save(bookmarkFromDB.get()));
+        }
+        return bookmarkFromDB;
+    }
+
 }
