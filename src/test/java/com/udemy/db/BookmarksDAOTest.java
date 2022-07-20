8b0cc80fedd40c6d2f3a67512d610613bde4905e
Jagadeesh Dachepalli <jagadeesh.dachepalli@oracle.com>
2022-07-20 16:37:12 +0530

    Add unittests for other functions of BookmarkDAO

--- src/test/java/com/udemy/db/BookmarksDAOTest.java
+++ src/test/java/com/udemy/db/BookmarksDAOTest.java
@@ -1,6 +1,7 @@
 package com.udemy.db;
 
 
+import com.google.common.base.Optional;
 import com.udemy.core.Bookmark;
 import com.udemy.core.User;
 import liquibase.Liquibase;
@@ -97,4 +98,108 @@ public class BookmarksDAOTest {
         Assert.assertNotNull(bookmarks);
         Assert.assertEquals(user.getId(), bookmarks.get(0).getUser().getId());
     }
+
+    @Test
+    public void testFindById() {
+        Optional<Bookmark> bookmark;
+        try {
+            ManagedSessionContext.bind(session);
+            tx = session.beginTransaction();
+
+            bookmark = bookmarkDAO.findById(1);
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            ManagedSessionContext.unbind(SESSION_FACTORY);
+            session.close();
+        }
+
+        Assert.assertNotNull(bookmark);
+        Assert.assertTrue(bookmark.isPresent());
+    }
+
+    @Test
+    public void testSave() {
+        Bookmark upBbookmark, downBookmark;
+        User user;
+        try {
+            ManagedSessionContext.bind(session);
+            tx = session.beginTransaction();
+
+            user = userDAO.findAll().get(0);
+            upBbookmark = new Bookmark("yahoo", "www.yahoo.com", "yahoo URL", user);
+            bookmarkDAO.save(upBbookmark);
+
+            downBookmark = bookmarkDAO.findById(upBbookmark.getId()).get();
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            ManagedSessionContext.unbind(SESSION_FACTORY);
+            session.close();
+        }
+
+        Assert.assertNotNull(downBookmark);
+        Assert.assertEquals(downBookmark.getUser().getId(), user.getId());
+    }
+
+    @Test
+    public void testDelete() {
+        List<Bookmark> bookmarks;
+        try {
+            ManagedSessionContext.bind(session);
+            tx = session.beginTransaction();
+
+            bookmarkDAO.delete(2);
+            bookmarks = bookmarkDAO.findForUser(1);
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            ManagedSessionContext.unbind(SESSION_FACTORY);
+            session.close();
+        }
+
+        Assert.assertNotNull(bookmarks);
+        Assert.assertEquals(1, bookmarks.size());
+    }
+
+    @Test
+    public void testDeleteBookmarksOfUser() {
+        List<Bookmark> bookmarks;
+        try {
+            ManagedSessionContext.bind(session);
+            tx = session.beginTransaction();
+
+            bookmarkDAO.deleteBookmarksOfUser(1);
+            bookmarks = bookmarkDAO.findForUser(1);
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            ManagedSessionContext.unbind(SESSION_FACTORY);
+            session.close();
+        }
+
+        Assert.assertNotNull(bookmarks);
+        Assert.assertEquals(0, bookmarks.size());
+    }
+
 }
